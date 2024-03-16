ab891d20f9b3563232493a1fb47a5b967cf19c37
Marline John <marlinejohn@gmail.com>
2024-03-16 23:44:27 +0100

    Add boostrap components and grid

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,18 +3,18 @@ import { Button, Card } from "react-bootstrap";
 
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <Card>
-        <Card.Img variant="top" src={movie.image}  />
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.director}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">
-            Open
-          </Button>
-        </Card.Body>
-      </Card>
-    );
+  return (
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image}  />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
+  );
   };
 
   MovieCard.propTypes = {
